89366e255918b64264e0e8946338b5d0e0201826
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2022-09-09 16:24:20 +0200

    Version avec ajout icone pour onglet de la page Web + essai javascript qui ne fonctionne pas encore

---
+++ javascript/debut.js
@@ -0,0 +1,32 @@
+var min=1; 
+var max=10;  
+var random = Math.floor(Math.random() * (max - min)) + min;
+
+let random = random;
+
+if(random = 1) {
+  alert(un);
+} else if (random = 2){
+  alert(deux);
+} else if (random = 3){
+  alert(trois);
+} else if (random = 4){
+  alert(quatre);
+} else if (random = 5){
+  alert(deux);
+} else if (random = 6){
+  alert(trois);
+} else if (random = 7){
+  alert(quatre);
+} else if (random = 8){
+  alert(trois);
+} else if (random = 9){
+  alert(quatre);
+}
+
+
+
+  console.log('deux')
+alert(random);
+}
+
